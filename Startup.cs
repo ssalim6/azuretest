69304ee496a6fa8f7dffd3e17e0195a837b3cdcb
ssalim <ssalim6@csc.com>
2019-01-08 16:38:59 +1100

    fix typo connect to sqldb in azure

--- Startup.cs
+++ Startup.cs
@@ -32,10 +32,10 @@ namespace DotNetCoreSqlDb
 			
 			// Use SQL Database if in Azure, otherwise,
 			//use SQLite
-			if(Environment.GetEnvironmentVariable(“ASPNETCORE_ENVIRONMENT”) == “Production”)
-				services.AddDbContext<MyDatabaseContext>(options => options.UseSqlServer(Configuration.GetConnectionString(“MyDbConnection”)));
+			if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+				services.AddDbContext<MyDatabaseContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
 			else
-			services.AddDbContext<MyDatabaseContext>(options => options.UseSqlite(“DataSource=localdatabase.db”));
+			services.AddDbContext<MyDatabaseContext>(options => options.UseSqlite("DataSource=localdatabase.db"));
 		
 			// Automatically perform database migration
 			services.BuildServiceProvider().GetService<MyDatabaseContext>().Database.Migrate();			
